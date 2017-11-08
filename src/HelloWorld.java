6f412c6788d41d49a408bfe8aad4ef9c16c6cf07
spormann <david.spormann@gmx.de>
2017-11-08 12:28:48 +0100

    addedHelloUser

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
-public class HelloWorld {
+public class HelloWorld
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 		System.out.println("Hello ASE2017 - How are you? :)");
+		
+		System.out.print("Please enter you name: ");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		HelloUser user = new HelloUser(input);
+		
+		user.greetUser();
 	}
 
 }
